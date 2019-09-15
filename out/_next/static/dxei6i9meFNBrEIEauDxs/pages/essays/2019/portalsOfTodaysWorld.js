9136d6ba3339f4a08c2b970dcd1dc6f897fb40f0
anshulkharb <help.geniustiger@gmail.com>
2019-09-15 12:58:16 +0530

    Static Site

---
+++ out/_next/static/dxei6i9meFNBrEIEauDxs/pages/essays/2019/portalsOfTodaysWorld.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{kUFq:function(n,o){},wGqI:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/essays/2019/portalsOfTodaysWorld",function(){var n=w("kUFq");return{page:n.default||n}}])}},[["wGqI",0]]]);
