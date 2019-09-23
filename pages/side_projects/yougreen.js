b738b21360ff21352b953782038767da1d5a9353
anshulkharb <help.geniustiger@gmail.com>
2019-09-23 20:05:10 +0530

    YouGreen new changes

--- pages/side_projects/yougreen.js
+++ pages/side_projects/yougreen.js
@@ -5,6 +5,7 @@ import Head from 'next/head'
 import { NextSeo } from 'next-seo';
 import SideProjects from '../../containers/sideProjects';
 import GoHome from '../../components/goHome'
+import Divider from '@material-ui/core/Divider';
 
 
 const YouGreen = () => {
@@ -30,20 +31,21 @@ const YouGreen = () => {
                 <Grid container item xs={8} md={6} lg={4} alignItems="flex-start" justify="center" direction="column">
 
                     
-                    <Box component='h1'>YouGreen: Save the planet one video at a time</Box>
+                    <Box component='div' fontSize="3em" >YouGreen: Save the planet one video at a time</Box><br />
 
 
 
-                    <img src='/static/side_projects/yougreen.png' style={{ width: '100%' }}></img>
+                    <img src='/static/side_projects/main.png' style={{ width: '100%' }}></img><br />
 
 
-                   
                     <Box component="span" color="grey"> 22 Sep '19</Box><br />
 
 
                     <Box component="h4">
                         You can download extnesion from <a href="" target="_blank">here</a>. <a href="https://github.com/anshulkharb/YouGreen" target="_blank">Github</a>
                     </Box>
+
+
 
                     <Box component='p'>
                     From cat videos to live game streaming, the world loves to watch videos on YouTube. YouTube has a variety of categories of videos, but music dominates them 
@@ -55,7 +57,7 @@ const YouGreen = () => {
                     <Box component="p">
                     Now let's jump into a different story. Some days ago, I came across <a href="https://www.emergeinteractive.com/insights/detail/does-irresponsible-web-development-contribute-to-global-warming/" target="_blank">this article</a>
                     </Box>
-                    <Box component="blockquote">
+                    <Box component="blockquote" style={{ fontSize: '1.2em', fontStyle: 'italic' }}>
                     According to the American Council for an Energy-Efficient Economy it takes 5.12 kWh of electricity per gigabyte of transferred data. And according to the Department of Energy 
                     the average US power plant expends 600 grams of carbon dioxide for every kWh generated. That means that transferring 1GB of data produces 3kg of CO2.
                     </Box>
@@ -67,7 +69,7 @@ const YouGreen = () => {
                     <Box component="p">
                     Now let's check some other numbers, <a href="https://help.netflix.com/en/node/87" target="_blank">according to Netflix</a>.
                     </Box>
-                    <Box component="blockquote">
+                    <Box component="blockquote" style={{ fontSize: '1.2em', fontStyle: 'italic' }}>
                     Watching TV shows or movies on Netflix uses about 1 GB of data per hour for each stream of standard definition video, and up to 3 GB per hour for each stream of HD video.
                     </Box><br />
 
@@ -76,20 +78,22 @@ const YouGreen = () => {
                     </Box><br />
 
                     <Box component="p">
-                    Now, coming back to the problem.
+                    
                     </Box>
 
                     <Box component="p">
-                    We know that music videos are still running continuously in the background tab(since I start writing this article) while fetching 100s of MB on full HD quality, but what if 
+                    Now, coming back to the problem, we know that music videos are still running continuously in the background tab(since I start writing this article) while fetching 100s of MB on full HD quality, but what if 
                     there's a way in which we can still listen to the music in the same quality by restricting video quality to the lowest, which we were not watching anyway and it'll function 
                     by fetching just fraction of the data compared to former way.
                     </Box>
 
                     <Box component="p">
-                    So, here's the simple solution, YouGreen, saving the planet one video at a time.
+                    So, here's the simple solution, YouGreen: saving the planet one video at a time.
                     </Box><br />
+                    <img src='/static/side_projects/yougreen.png' style={{ width: '100%' }}></img>
 
-                    <Box component="p">
+
+                    <Box component="h4">
                     Here's it how it works:
                     </Box>
 
@@ -98,6 +102,11 @@ const YouGreen = () => {
                     to that tab, that was playing music in background, and play some other video manually, YouGreen will intelligently upgrade video quality.
                     </Box>
 
+                    <Divider />
+
+                    <Box component="h3">
+                        For collaboration or anything else, shoot mail at hi@anshulkharb.com
+                    </Box>
 
                 </Grid>
                 <Grid item xs={2} md={3} lg={4}></Grid>
