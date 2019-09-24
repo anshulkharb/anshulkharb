fa1d6647b5af0f0e1d9483e24e9f0d0a8a11e21c
anshulkharb <help.geniustiger@gmail.com>
2019-09-24 12:50:46 +0530

    GOhOME BUTTON

--- components/goHome.js
+++ components/goHome.js
@@ -1,15 +1,27 @@
 import React, { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Link from 'next/link';
+import Hidden from '@material-ui/core/Hidden';
 
 
 const GoHome = () => {
 
     return (
-        <div style={{position: 'fixed', top: '3vh', left: '3vh', zIndex: '100', textDecoration: 'none'}}>
-            <Link href='/'>
-                <a><img src='/static/icons/home.png'></img></a>
-            </Link>
+        <div>
+            <Hidden smDown>
+                <div style={{ position: 'fixed', top: '3vh', left: '12vW', zIndex: '100', textDecoration: 'none' }}>
+                    <Link href='/'>
+                        <a><img src='/static/icons/home.png'></img></a>
+                    </Link>
+                </div>
+            </Hidden>
+            <Hidden mdUp>
+                <div style={{ position: 'fixed', top: '2vh', left: '1.5vh', zIndex: '100', textDecoration: 'none' }}>
+                    <Link href='/'>
+                        <a><img src='/static/icons/home.png'></img></a>
+                    </Link>
+                </div>
+            </Hidden>
         </div>
     );
 };
