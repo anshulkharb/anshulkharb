d1053e2bab844bbf98991ab1317fe00737565b8a
anshulkharb <help.geniustiger@gmail.com>
2019-09-25 18:31:30 +0530

    v 1.5

--- pages/side_projects/yougreeninstalled.js
+++ pages/side_projects/yougreeninstalled.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Head from 'next/head'
 
 
 const YouGreenInstalled = () => {
 
     return (
         <div>
+        <Head>
+            <title>Thanks for intalling YouGreen: Saving the planet one video at a time</title>
+            <link rel="icon" type="image/x-icon" href="/static/icons/AK.png" />
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
+            <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5d88f23643af7d44"></script>
+
+        </Head>
             <h2>Thanks for installing YouGreen</h2>
             <a href="https://anshulkharb.com/side_projects/yougreen" target="_blank">Share this extension with your friends to save data and environment.</a>
             
